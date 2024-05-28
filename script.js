5263e1e33210572d0fd9109e8ef0d46788c8cd73
correia1302 <161593384+correia1302@users.noreply.github.com>
2024-05-28 10:08:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-perguntas");
+const caixaAlternativas=document.querySelector(".caixa-alternetivas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
 
+const perguntas=[
+  {
+     enunciado: "você gosta de ir ao cinema?",
+     alternativas:[
+       "sim",
+       "não",
+    ]
+  }
+{
+     enunciado: "você prefere pizza ou lasanha?",
+     alternativas:[
+       "lasanha",
+       "pizza",
+    ]
+  }
+{
+     enunciado: "você gosta de futebol?",
+     alternativas:[
+       "sim",
+       "não",
+    ]
+  }
+{
+     enunciado: "você tem filhos?",
+     alternativas:[
+       "sim",
+       "não",
+    ]
+  }
+{
+     enunciado: "você tem animais de estimação?",
+     alternativas:[
+       "sim",
+       "não",
+    ]
+  }
+];
+ 
+ 
